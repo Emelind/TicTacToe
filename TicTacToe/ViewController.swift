042a8c796822977d051a665506c038bd3c643c87
emelie <emeliejlindvall@gmail.com>
2020-12-15 14:36:23 +0100

    Added image files and placed them in Storyboard

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func action(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
